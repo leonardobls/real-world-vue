9e9e0d245a473a31cda0867a9104b5ba58ad1950
leonardobls <leonardobitencourt2020@gmail.com>
2024-12-06 16:28:53 -0300

    Exemplo de uso do SASS dentro de um projeto vue2

---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="my-component">
+        <h4>{{ title }}</h4>
+        <div class="quadrado"></div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            title: 'Park Cleanup',
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.my-component {
+    background: red;
+    h4 {
+        color: $blue;
+    }
+
+    .quadrado {
+        width: 100px;
+        height: 100px;
+        background: blue;
+    }
+}
+</style>
